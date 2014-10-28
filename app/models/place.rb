e793b0e701b888cb7636dc8b3a20512a5053f4bb
Harry Ng <harryworld@gmail.com>
2014-10-28 16:18:34 +0800

    6. Geocoding the latitude/longitude

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,6 @@
 class Place < ActiveRecord::Base
+  reverse_geocoded_by :lat, :lng
+  after_validation :reverse_geocode  # auto-fetch address
+  geocoded_by :address, :latitude  => :lat, :longitude => :lng   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
 end
